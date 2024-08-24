293a3a146bcdef63eb97ba78e7684bb5a4d065f5
Kyblue11 <142404373+Kyblue11@users.noreply.github.com>
2024-08-24 09:45:39 +0800

    Req1: Tarnished
    - Modified the toString method of Tarnished to re-use the addAttribute and setAttribute methods of its abstract superclass
    - Created new enum class TarnishedActorAttributes.java to cater for new property of Tarnished, STRENGTH

--- src/game/Tarnished.java
+++ src/game/Tarnished.java
@@ -3,6 +3,8 @@ package game;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttribute;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 
@@ -12,19 +14,30 @@ public class Tarnished extends Player {
 
     public Tarnished(String name, char displayChar) {
         super(name, displayChar, 150);
-        this.mana = 100;
-        this.strength = 5;
+        this.addAttribute(BaseActorAttributes.MANA, new BaseActorAttribute(100));
+        //this.addAttribute(BaseActorAttributes.STRENGTH, new BaseActorAttribute(5));
     }
 
-    @Override
-    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        display.println("Hitpoints: " + super.toString());
-        display.println("Mana: " + this.mana);
-        display.println("Strength: " + this.strength);
-        return super.playTurn(actions, lastAction, map, display);
+//    @Override
+//    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+//        display.println("Hitpoints: " + this.getAttribute(BaseActorAttributes.HEALTH) + "/" + this.getAttributeMaximum(BaseActorAttributes.HEALTH));
+//        display.println("Mana: " + this.getAttribute(BaseActorAttributes.MANA) + "/" + this.getAttributeMaximum(BaseActorAttributes.MANA));
+//        //display.println("Strength: " + this.getAttribute(GameActorAttributes.STRENGTH) + "/" + this.getAttributeMaximum(GameActorAttributes.STRENGTH));
+//        return super.playTurn(actions, lastAction, map, display);
+//    }
+
+    public int getMana() {
+        return mana;
     }
 
     public int getStrength() {
         return strength;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " , "
+                + "Mana: " + this.getAttribute(BaseActorAttributes.MANA) + "/" + this.getAttributeMaximum(BaseActorAttributes.MANA) + " , ";
+                //+ "Strength: " + this.getAttribute(BaseActorAttributes.STRENGTH) + "/" + this.getAttributeMaximum(BaseActorAttributes.STRENGTH);
     }
 }
