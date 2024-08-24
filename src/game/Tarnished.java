2854bbb28b3dcbec5ff1d72754167ed550365b7d
alam0055 <alam0055@student.monash.edu>
2024-08-24 13:44:19 +0800

    Req1: Tarnished.java
    - Modified Tarnished class to handle STRENGTH attribute
    - Since TarnishedActorAttributes also implements ActorAttribute, Tarnish can invoke addAttribute and getAttribute

--- src/game/Tarnished.java
+++ src/game/Tarnished.java
@@ -7,37 +7,31 @@ import edu.monash.fit2099.engine.actors.attributes.BaseActorAttribute;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tarnished extends Player {
-    private int mana;
-    private int strength;
 
     public Tarnished(String name, char displayChar) {
         super(name, displayChar, 150);
         this.addAttribute(BaseActorAttributes.MANA, new BaseActorAttribute(100));
-        //this.addAttribute(BaseActorAttributes.STRENGTH, new BaseActorAttribute(5));
+        this.addAttribute(TarnishedActorAttributes.STRENGTH, new BaseActorAttribute(5));
     }
 
-//    @Override
-//    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-//        display.println("Hitpoints: " + this.getAttribute(BaseActorAttributes.HEALTH) + "/" + this.getAttributeMaximum(BaseActorAttributes.HEALTH));
-//        display.println("Mana: " + this.getAttribute(BaseActorAttributes.MANA) + "/" + this.getAttributeMaximum(BaseActorAttributes.MANA));
-//        //display.println("Strength: " + this.getAttribute(GameActorAttributes.STRENGTH) + "/" + this.getAttributeMaximum(GameActorAttributes.STRENGTH));
-//        return super.playTurn(actions, lastAction, map, display);
-//    }
-
     public int getMana() {
-        return mana;
+        return this.getAttribute(BaseActorAttributes.MANA);
     }
 
     public int getStrength() {
-        return strength;
+        return this.getAttribute(TarnishedActorAttributes.STRENGTH);
     }
 
     @Override
     public String toString() {
         return super.toString() + " , "
-                + "Mana: " + this.getAttribute(BaseActorAttributes.MANA) + "/" + this.getAttributeMaximum(BaseActorAttributes.MANA) + " , ";
-                //+ "Strength: " + this.getAttribute(BaseActorAttributes.STRENGTH) + "/" + this.getAttributeMaximum(BaseActorAttributes.STRENGTH);
+                + "Mana: (" + this.getAttribute(BaseActorAttributes.MANA) + "/" + this.getAttributeMaximum(BaseActorAttributes.MANA) + " ) "
+                + "Strength: (" + this.getAttribute(TarnishedActorAttributes.STRENGTH) + "/" + this.getAttributeMaximum(TarnishedActorAttributes.STRENGTH) + " )";
     }
 }
