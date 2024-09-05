ad4277f8e5f64a1fc6eeac49d5fc1cc378bdc0b2
alam0055 <alam0055@student.monash.edu>
2024-09-05 15:48:13 +0800

    Req1: Weapons
    - Added documentation for weapons ShortSword.java and GreatKnife.java

--- src/game/weapons/weaponitems/ShortSword.java
+++ src/game/weapons/weaponitems/ShortSword.java
@@ -2,6 +2,9 @@ package game.weapons.weaponitems;
 
 public class ShortSword extends WeaponItem {
 
+    /**
+     * Short Sword constructor.
+     */
     public ShortSword() {
         super("Short Sword", '!', 100, "slashes", 75, 10);
     }
